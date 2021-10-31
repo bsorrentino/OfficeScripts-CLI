8ef49007bdd12333fba0faa392b736c8bccb2e0d
bsorrentino <bartolomeo.sorrentino@gmail.com>
2021-10-31 11:12:38 +0100

    refactor(clean): remove unused file

--- upload.mjs
+++
@@ -1,3 +0,0 @@
-
-
-await $`m365 spo file add --webUrl https://softphone-my.sharepoint.com/personal/bartolomeo_sorrentino_softphone_it/ --folder 'Documents/Documents/Office Scripts' --path 'FillWeek.osts'`
